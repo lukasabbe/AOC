11491ba43a9d2c74505c1bdbb309fbebd3f29899
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-01 15:11:53 +0100

    improvments

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -1,21 +1,9 @@
+from util import readData
+
 def getTotal(leftList, rightList):
     leftList.sort()
     rightList.sort()
-    total = 0
-    for i in range(len(leftList)):
-        total += abs(leftList[i] - rightList[i])
-    return total
-
-def readData():
-    leftList = []
-    rightList = []
-    with open("./day1/day1_values.txt", "r") as file:
-        for line in file:
-            left = line.split()[0]
-            right = line.split()[-1]
-            leftList.append(int(left))
-            rightList.append(int(right))
-    return leftList, rightList
+    return sum([abs(leftList[i] - rightList[i]) for i in range(len(leftList))])
 
 if __name__ == "__main__":
     leftList, rightList = readData()
--- day1/day1_part2.py
+++ day1/day1_part2.py
@@ -1,29 +1,10 @@
-def readData():
-    leftList = []
-    rightList = []
-    with open("./day1/day1_values.txt", "r") as file:
-        for line in file:
-            left = line.split()[0]
-            right = line.split()[-1]
-            leftList.append(int(left))
-            rightList.append(int(right))
-    return leftList, rightList
+from util import readData
 
 def findAmountOf(num, rightlist):
-    amount = 0
-    for i in range(len(rightlist)):
-        if rightlist[i] == num:
-            amount += 1
-    return amount
-
+    return sum(1 for x in rightlist if x == num)
 
 def getTotal(leftList, rightList):
-    total = 0
-    for i in range(len(leftList)):
-        amount = findAmountOf(leftList[i], rightList)
-        total += amount * leftList[i]
-    return total
-
+    return sum([findAmountOf(num, rightList) * num for num in leftList])
 
 if __name__ == "__main__":
     leftList, rightList = readData()
