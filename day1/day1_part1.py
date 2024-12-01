01be4fc2f2c6894f5aeceeb3060f1c7fac1473b8
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-01 14:44:38 +0100

    made solution 1 beter

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -1,21 +1,15 @@
-def findMinPair(leftList, rightList):
-    leftMinValue = min(leftList)
-    rightMinValue = min(rightList)
-    return abs(leftMinValue - rightMinValue), leftMinValue, rightMinValue
-
 def getTotal(leftList, rightList):
+    leftList.sort()
+    rightList.sort()
     total = 0
     for i in range(len(leftList)):
-        pair = findMinPair(leftList, rightList)
-        total += pair[0]
-        leftList.remove(pair[1])
-        rightList.remove(pair[2])
+        total += abs(leftList[i] - rightList[i])
     return total
 
 def readData():
     leftList = []
     rightList = []
-    with open("day1_values.txt", "r") as file:
+    with open("./day1/day1_values.txt", "r") as file:
         for line in file:
             left = line.split()[0]
             right = line.split()[-1]
