ef518d62982c364efb84726777c8008767e597a8
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-02 07:36:42 +0100

    Rewriten

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -1,28 +1,20 @@
 from util import readData
 
-
-def isSafe(line):
-    splitLine = line.split(" ")
+def isSafe(line : list):
     safeCount = True
-    incrising = False
-    decrising = False
-    for i in range(len(splitLine)):
-        if(len(splitLine) - 1 != i):
-            if(abs(int(splitLine[i]) - int(splitLine[i+1])) > 3):
-                safeCount = False
-            if(int(splitLine[i]) - int(splitLine[i+1]) < 0):
-                decrising = True
-            elif(int(splitLine[i]) - int(splitLine[i+1]) == 0):
-                safeCount = False
-            else:
-                incrising = True
-    if(incrising and decrising):
-        safeCount = False
-    if(not incrising and not decrising):
+    counter = 0
+    for i in range(len(line)-1):
+        if(abs(int(line[i]) - int(line[i+1])) > 3):
+            safeCount = False
+        if(int(line[i]) - int(line[i+1]) < 0):
+            counter-=1
+        elif int(line[i]) - int(line[i+1]) > 0:
+            counter+=1
+    if abs(counter) != len(line)-1:
         safeCount = False
     return safeCount
 
 if __name__ == "__main__":
     #lines = ["7 6 4 2 1","1 2 7 8 9","9 7 6 2 1","1 3 2 4 5","8 6 4 4 1","1 3 6 7 9"]
     lines = readData()
-    print(sum(1 for line in lines if isSafe(line)))
+    print(sum(1 for line in lines if isSafe(line.split(" "))))
--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,43 +1,19 @@
 from util import readData
+from day2_part1 import isSafe
 
-
-def isSafe(splitLine:list):
-    safeCount = True
-    incrising = False
-    decrising = False
-    for i in range(len(splitLine)):
-        if(len(splitLine) - 1 != i):
-            if(abs(int(splitLine[i]) - int(splitLine[i+1])) > 3):
-                safeCount = False
-            if(int(splitLine[i]) - int(splitLine[i+1]) < 0):
-                decrising = True
-            elif(int(splitLine[i]) - int(splitLine[i+1]) == 0):
-                safeCount = False
-            else:
-                incrising = True
-    if(incrising and decrising):
-        safeCount = False
-    if(not incrising and not decrising):
-        safeCount = False
-    if safeCount:
-        print(splitLine)
-    return safeCount
+def checkLine(line:list):
+    if(isSafe(line)):
+        return True
+    else:
+        for i in range(len(line)):
+            copyOfList = line.copy()
+            copyOfList.pop(i)
+            if(isSafe(copyOfList)):
+                return True
+    return False
 
 if __name__ == "__main__":
     #lines = ["7 6 4 2 1","1 2 7 8 9","9 7 6 2 1","1 3 2 4 5","8 6 4 4 1","1 3 6 7 9"]
     lines = readData()
-
-    safeList = []
-    for line in lines:
-        line = line.split(" ")
-        if(isSafe(line)):
-            safeList.append(1)
-        else:
-            for i in range(len(line)):
-                copyOfList = line.copy()
-                copyOfList.pop(i)
-                if(isSafe(copyOfList)):
-                    safeList.append(1)
-                    break
         
-    print(sum(1 for line in safeList))
+    print(sum(1 for line in lines if checkLine(line.split(" "))))
