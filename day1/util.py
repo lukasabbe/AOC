288693fb7639c65b85154857f672c81d4393b3eb
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-02 07:06:12 +0100

    that went horible

--- day1/util.py
+++ day1/util.py
@@ -3,8 +3,7 @@ def readData():
     rightList = []
     with open("./day1/day1_values.txt", "r") as file:
         for line in file:
-            left = line.split()[0]
-            right = line.split()[-1]
-            leftList.append(int(left))
-            rightList.append(int(right))
+            lineSplit = line.split()
+            leftList.append(int(lineSplit[0]))
+            rightList.append(int(lineSplit[-1]))
     return leftList, rightList
