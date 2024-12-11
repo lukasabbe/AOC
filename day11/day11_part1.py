26bbf11e09357fc343cbd96606e4d9df3bd1d043
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-11 07:23:12 +0100

    day11

---
+++ day11/day11_part1.py
@@ -0,0 +1,25 @@
+import sys, os
+def getRule(num):
+    if num == 0:
+        return [1]
+    elif(len(str(num)) % 2 == 0):
+        return [int(str(num)[:len(str(num))//2]), int(str(num)[len(str(num))//2:])]
+    else:
+        return [num * 2024]
+
+def calcRules(nums):
+    nums = nums.split(" ")
+    rules = []
+    for num in nums:
+        rules.append(getRule(int(num)))
+    return " ".join([str(num) for rule in rules for num in rule])
+
+if(__name__ == "__main__"):
+    current = "8069 87014 98 809367 525 0 9494914 5"
+    for i in range(75):
+        print("on iteration: ", i)
+        current = calcRules(current)
+        os.system("cls")
+    print(current)
+    print(len(current.split(" ")))
+    
