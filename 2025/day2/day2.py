1afc966620eb6f8525fe155346bbfbac051a38fe
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-12-02 10:30:26 +0100

    Made it more readable

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,38 +1,23 @@
-def isVaildId(id: str):
-    for check in range(1, len(id) // 2 + 1):
-        pattern = id[0:check]
-        vaild = False
-        for i in range(check, len(id), check):
-            if id[i : i + check] != pattern:
-                vaild = True
-                break
-        if not vaild:
-            return False
-    return True
-
-
-def isVaildIdOne(id: str):
-
+def part1(id: str):
     if len(id) % 2 != 0:
         return True
-
     if id[len(id) // 2 :] == id[0:len(id) // 2]:
         return False
     return True
 
-def part1():
-    ids = open("day2/input.txt").read().split(",")
-    total = 0
-    for id in ids:
-        numOne = int(id.split("-")[0].strip())
-        numTwo = int(id.split("-")[1].strip())
-        for i in range(numOne, numTwo + 1):
-            iStr = str(i)
-            if not isVaildIdOne(iStr):
-                total += int(iStr)
-    print(total)
+def part2(id: str):
+    for check in range(1, len(id) // 2 + 1):
+        pattern = id[0:check]
+        valid = False
+        for i in range(check, len(id), check):
+            if id[i : i + check] != pattern:
+                valid = True
+                break
+        if not valid:
+            return False
+    return True
 
-def part2():
+def checkValid(func):
     ids = open("day2/input.txt").read().split(",")
     total = 0
     for id in ids:
@@ -40,10 +25,12 @@ def part2():
         numTwo = int(id.split("-")[1].strip())
         for i in range(numOne, numTwo + 1):
             iStr = str(i)
-            if not isVaildId(iStr):
+            if not func(iStr):
                 total += int(iStr)
     print(total)
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    #part 1
+    checkValid(part1)
+    #part 2
+    checkValid(part2)
